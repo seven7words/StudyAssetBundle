ed13f017956988be5c0b6fa7eb34dcc2a6091234
woopqww111 <woopqww111@yahoo.com>
2017-09-20 19:20:07 +0800

    随便

---
+++ StudyAssetBundle/Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using System.IO;
+public class CreateAssetBundles  {
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles(){
+        string dir = "AssetsBundles";
+        if (Directory.Exists(dir) == false){
+            Directory.CreateDirectory(dir);
+        }
+        BuildPipeline.BuildAssetBundles(dir,BuildAssetBundleOptions.None,BuildTarget.StandaloneWindows64);
+    }
+}
---
+++ StudyAssetBundle/Assets/LoadFromFileExample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.Networking;
+public class LoadFromFileExample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string dir = "AssetsBundles";
+        AssetBundle ab =  AssetBundle.LoadFromFile(dir+"/scene/wall.unity3d");
+        //GameObject go =  ab.LoadAsset<GameObject>("Cube");
+        //Instantiate(go);
+        Object[] objs = ab.LoadAllAssets();
+        foreach(Object o in objs){
+            Instantiate(o);
+        }
+	}
+
+	
+
+}
